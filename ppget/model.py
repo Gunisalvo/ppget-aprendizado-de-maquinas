1f9fbfa9e7b2673636d1815c702b610da80a0d0d
Rodrigo Leite <rodrigoleite@Rodrigos-MacBook-Pro-2.local>
2022-06-27 13:41:57 +0100

    Evaluation

---
+++ ppget/model.py
@@ -0,0 +1,71 @@
+import os
+import numpy as np
+from keras.layers import Dense, Dropout, Conv1D, Flatten, InputLayer, LSTM, GRU
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+def __CNN1D(model, neurons, dropout):
+    model.add(Conv1D(neurons, kernel_size=4, activation="relu"))
+    model.add(Dropout(dropout))
+    model.add(Flatten())
+    model.add(Dense(neurons, "relu"))
+    model.add(Dense(1, "linear"))
+
+
+def __LSTM(model, neurons, dropout):
+    model.add(LSTM(neurons, return_sequences=False, dropout=dropout))
+    model.add(Dense(neurons, 'relu'))
+    model.add(Dense(1, 'linear'))
+
+
+def __GRU(model, neurons, dropout):
+    model.add(GRU(neurons))
+    model.add(Dropout(dropout))
+    model.add(Dense(neurons, 'relu'))
+    model.add(Dense(1, 'linear'))
+
+
+MODEL_STRUCTURE = {
+    "cnn1d": __CNN1D,
+    "lstm": __LSTM,
+    "gru": __GRU
+}
+
+
+class Model:
+
+    def __init__(self, name, model_type, window, neurons=8, dropout=0.02, patience=4, monitor="val_loss"):
+
+        self.model = Sequential()
+        self.model.add(InputLayer((window - 1, 1)))
+        self.name = name
+
+        MODEL_STRUCTURE[model_type](self.model, neurons, dropout)
+        model_file_name = os.path.join(f"model/{self.name}-{model_type}", f"w{window}n{neurons}d{dropout}.model")
+        self.callbacks = [
+            EarlyStopping(monitor=monitor, patience=patience),
+            ModelCheckpoint(filepath=model_file_name, monitor=monitor, save_best_only=True)
+        ]
+
+    def compile(self, learning_rate=0.0001, loss_function="mse"):
+        self.model.compile(loss=loss_function, optimizer=Adam(learning_rate=learning_rate, amsgrad=True))
+
+    def fit(self, X, y, batch_size=64, validation_split=0.2, epochs=100):
+        self.model.fit(
+            X,
+            y,
+            validation_split=validation_split,
+            epochs=epochs,
+            batch_size=batch_size,
+            callbacks=self.callbacks
+        )
+
+    def predict(self, data):
+        predicted = self.model.predict(data)
+        predicted = np.reshape(predicted, (predicted.size,))
+        return predicted
+
+    def describe(self):
+        self.model.summary()
--- ppget/source.py
+++ ppget/source.py
@@ -1,63 +1,164 @@
 import pandas as pd
-import shutil
-import os
+import numpy as np
+from functools import reduce
 
 
-class ModelSource:
+class DataSource:
 
-    __structure = {
-        "Time": str,
-        "Date": str,
-        "Global_active_power": float,
-        "Global_reactive_power": float,
-        "Voltage": float,
-        "Global_intensity": float,
-        "Sub_metering_1": float,
-        "Sub_metering_2": float,
-        "Sub_metering_3": float
-    }
+    def __init__(self, filename, size=(30*24*60), step=0.1, sample_frequency="10min", features=None):
+        if features is None:
+            features = ["Global_active_power"]
+        self.baseframe = pd.read_csv(filename, delimiter=";", na_values="?", iterator=True)
+        self.index = 0
+        self.size = size
+        self.step = step
+        self.sample_frequency = sample_frequency
+        self.features = features
+        self.frame = pd.DataFrame()
 
-    __datasource = None
+    def __iter__(self):
+        return self
 
-    def __init__(self, start_date, end_date, feature, frequency, path, cleaning_backfill):
-        def seasonal_fill(s):
-            return s.fillna(s.shift(-24 * cleaning_backfill))
+    def _next_frame(self):
+        if self.frame.empty:
+            dataframe = self.baseframe.get_chunk(self.size)
+            step_size = 0
+        else:
+            step_size = int(self.size * self.step)
+            dataframe = self.baseframe.get_chunk(step_size)
 
-        def clean_data(dataframe):
-            date_range = (dataframe["date_filter"] >= start_date) & (dataframe["date_filter"] < end_date)
-            return dataframe.dropna(subset=[feature]).loc[date_range][feature].resample(frequency).mean()
+        dataframe = dataframe.fillna(dataframe.median(numeric_only=True))
+        dataframe["timestamp"] = pd.to_datetime(dataframe["Date"] + " " + dataframe["Time"], format="%d/%m/%Y %H:%M:%S")
+        dataframe = dataframe.set_index("timestamp")
 
-        data = pd.read_csv(path,
-                           header=0,
-                           squeeze=True,
-                           delimiter=";",
-                           na_values=["?"],
-                           dtype=self.__structure)
+        def resample_feature(acc, feature):
+            frame = dataframe[feature].resample(self.sample_frequency).mean()
+            acc["index"] = frame.index
+            series = acc.get("series", [])
+            series.append(frame.tolist())
+            acc["series"] = series
+            return acc
 
-        data["timestamp"] = pd.to_datetime(data["Date"] + " " + data["Time"], format="%d/%m/%Y %H:%M:%S")
-        data["date_filter"] = pd.to_datetime(data["Date"] + " " + data["Time"],
-                                             format="%d/%m/%Y %H:%M:%S")
-        data = data.set_index("timestamp")
-        self.__datasource = seasonal_fill(clean_data(data))
+        resampled_features = reduce(resample_feature, self.features, {})
 
-    def seasonal_replace_days(self, dates):
-        pass
+        frame = pd.DataFrame(resampled_features["series"]).T
+        frame.index = resampled_features["index"]
+        frame.columns = self.features
 
-    def missing_data(self):
-        return self.__datasource[self.__datasource.isnull()]
+        if not self.frame.empty and step_size > 0:
+            slide_size = len(self.frame.index) - len(frame.index)
+            first_part = self.frame.tail(slide_size)
+            result = pd.concat([first_part, frame])
+            result_index = np.concatenate([first_part.index, frame.index])
+            result.index = result_index
+        else:
+            result = frame
 
-    def series(self):
-        return self.__datasource
+        return result
 
-    @staticmethod
-    def load(start_date, end_date, feature,
-             frequency="10min", path="./data/household_power_consumption.txt", cleaning_backfill=1):
-        return ModelSource(start_date, end_date, feature, frequency, path, cleaning_backfill)
+    def __next__(self):
+        self.index += 1
+        self.frame = self._next_frame()
+        print(f"{self.frame.index[0]} -> {self.frame.index[-1]}")
+        return self
+
+    def generate_window(self, train_test_split, sequence_length):
+        return WindowGenerator(train_test_split, sequence_length, self.frame)
 
 
-def prepare_filesystem(model):
-    try:
-        shutil.rmtree(model)
-    except Exception as ex:
-        print(f"prepare_filesystem: ignoring - {ex}")
-    os.makedirs(f"{os.getcwd()}/{model}cluster")
+def normalise_windows(window_data, single_window=False):
+    normalised_data = []
+    window_data = [window_data] if single_window else window_data
+    for w in window_data:
+        normalised_window = []
+        for i in range(w.shape[1]):
+            normalised_column = [((float(p) / float(w[0, i])) - 1) for p in w[:, i]]
+            normalised_window.append(normalised_column)
+        normalised_window = np.array(normalised_window).T
+        normalised_data.append(normalised_window)
+    return np.array(normalised_data)
+
+
+class WindowGenerator:
+
+    def __init__(self, train_test_split, sequence_length, df):
+        self.sequence_length = sequence_length
+        self.split_size = int(len(df) * train_test_split)
+        self.train_frame = df.values[:self.split_size]
+        self.test_frame = df.values[self.split_size:]
+
+    def train_data(self, normalise):
+        X_train = []
+        y_train = []
+        for n in range(len(self.train_frame) - self.sequence_length):
+            X, y = self._next_window(n, normalise)
+            X_train.append(X)
+            y_train.append(y)
+        return np.array(X_train), np.array(y_train)
+
+    def _next_window(self, n, normalise):
+        window = self.train_frame[n:n + self.sequence_length]
+        window = normalise_windows(window, single_window=True)[0] if normalise else window
+        X = window[:-1]
+        y = window[-1, [0]]
+        return X, y
+
+    def test_data(self, normalise):
+        windows = []
+        for n in range(len(self.test_frame) - self.sequence_length):
+            windows.append(self.test_frame[n:n + self.sequence_length])
+        labels = np.array(windows).astype(float)
+        normalised_labels = normalise_windows(labels, single_window=False) if normalise else labels
+
+        X = normalised_labels[:, :-1]
+        y = normalised_labels[:, -1, [0]]
+        return X, y
+
+
+# if __name__ == "__main__":
+#     A_DAY = 60 * 24
+#     DAYS_OF_DATA = 30
+#
+#     WINDOW = 25
+#     BATCH_SIZE = 32
+#     LEARNING_RATE = 0.00075
+#
+#     s = DataSource("../data/household_power_consumption.txt", size=DAYS_OF_DATA * A_DAY, step=0.33)
+#     score_evolution = []
+#     score_evolution2 = []
+#
+#     online_model = Model(name="online", model_type="cnn1d", window=WINDOW)
+#     online_model.compile(LEARNING_RATE)
+#     for _ in range(0, 50):
+#         f = s.__next__()
+#
+#         window = f.generate_window(0.9, WINDOW)
+#         try:
+#             X_train, y_train = window.train_data(True)
+#             X_test, y_test = window.test_data(True)
+#         except Exception as _:
+#             break
+#         print(f"{X_train.shape}, {y_train.shape} : {X_test.shape}, {y_test.shape}")
+#
+#         batch_model = Model(name="batch", model_type="cnn1d", window=WINDOW)
+#         batch_model.compile(LEARNING_RATE)
+#
+#         online_model.fit(
+#             X_train,
+#             y_train,
+#             batch_size=BATCH_SIZE
+#         )
+#
+#         batch_model.fit(
+#             X_train,
+#             y_train,
+#             batch_size=BATCH_SIZE)
+#
+#         predictions = online_model.predict(X_test)
+#         predictions2 = batch_model.predict(X_test)
+#
+#         score_evolution.append(r2_score(predictions, y_test))
+#         score_evolution2.append(r2_score(predictions2, y_test))
+#
+#         plt.clf()
+
